3913caa630e2696287d6e3d3fb141ccbf853b4bd
Andrew Morris <jump3r@dggaming.net>
2014-11-22 05:55:32 -0500

    Update with player movement.

---
+++ Assets/Scripts/GuiManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour {
+
+	private int generation = 0; // Current Generation.
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetGeneration(int g) {
+		generation = g;
+	}
+
+	void OnGUI() {
+		GUI.Box (new Rect(Screen.width * 0.75f, Screen.height * 0.02f, Screen.width * .2f, Screen.height * 0.04f), "Current Generation: " + generation.ToString ());
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	private float moveSpeed = 4f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			//rigidbody.velocity = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+			transform.position = new Vector3 (transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y, -1f);
+			//Debug.Log ("Move Right please");
+			//RotateDirection (-90);
+		}
+		
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position = new Vector3 (transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y, -1f);
+			//RotateDirection (90);
+		}
+		
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y + (moveSpeed * Time.deltaTime), -1f);
+		}
+		
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y - (moveSpeed * Time.deltaTime), -1f);
+			//RotateDirection (180);
+		}
+	}
+}
